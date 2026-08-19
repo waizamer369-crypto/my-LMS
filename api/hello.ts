ad408fe172f953b9c01f4504c021aeb36e0e17f0
waiz-tech <waizamer369@gmail.com>
2026-08-20 02:14:06 +0500

    fix: vercel.json syntax, remove debug hello.ts

--- api/hello.ts
+++
@@ -1,5 +0,0 @@
-export default function handler(request: Request) {
-  return new Response(JSON.stringify({ message: "hello world" }), {
-    headers: { "content-type": "application/json" },
-  });
-}
